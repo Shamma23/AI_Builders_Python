7f623194e4d5122100e485572729d53595f2d650
Shamma23 <s.almansoori1@yahoo.com>
2026-09-10 22:36:21 +0400

    Basic dtypes of python

---
+++ basic_dtypes.py
@@ -0,0 +1,14 @@
+print("Hello")
+x = 1 #integer
+y = 1.5 #float
+z = "hello" #string
+V = 'helo'
+
+t = """This is a multi-line string.
+It can span multiple lines.
+"""
+
+u = True #boolean
+W = False #boolean
+
+print(type(W))
